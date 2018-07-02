e9a80073025146faec2326a14ec838439ea2a666
Gerben Oostra <gerben.oostra@bigdatarepublic.nl>
2018-07-02 21:12:48 +0200

    included building of generated project in travis

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -9,10 +9,11 @@ setup(
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
     long_description="README.md",
 {% if cookiecutter.python_interpreter == 'python3' %}
-    python_requires='3',
+    python_requires='>3.5',
 {% endif %}
     install_requires=[
                      "click",
+                     "python-dotenv>=0.5.1",
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"]
